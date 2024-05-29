61457e8f88f6f3784170e1da81b6cc028e24c1d9
Daniel16121990 <nukenabber834159672@gmail.com>
2024-05-29 14:32:37 -0400

    adicionando funcionalidad al boton de agendar en la iglesia

--- script.js
+++ script.js
@@ -129,3 +129,16 @@ window.onload = function() {
   overlay.style.display = 'none';
   abrirModal()
 };
+
+//agendar asistencia en la iglesia
+document.getElementById('btn-agendar-iglesia').addEventListener('click', function() {
+  var titulo = 'Ceremonia religiosa Antonio y Noemi';
+  var descripcion = 'Boda de Antonio y Nomei en la iglesia';
+  var ubicacion = 'Iglesia';
+  var fechaInicio = '2024-06-01T09:00:00';
+  var fechaFin = '2024-06-01T10:00:00';
+  
+  var url = `https://www.google.com/calendar/render?action=TEMPLATE&text=${encodeURIComponent(titulo)}&details=${encodeURIComponent(descripcion)}&location=${encodeURIComponent(ubicacion)}&dates=${fechaInicio.replace(/-|:|\.\d\d\d/g, '')}/${fechaFin.replace(/-|:|\.\d\d\d/g, '')}`;
+  
+  window.open(url, '_blank');
+});
